0631489bee7c63b9e60e4d4da4fe7e049d1ed34b
Dmitry <nakortahmail@gmail.com>
2016-05-12 02:09:53 +0300

    ы

--- gnoiniyProject/gnoiniyProject/App_Start/RouteConfig.cs
+++ gnoiniyProject/gnoiniyProject/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace gnoiniyProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "chat", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ gnoiniyProject/gnoiniyProject/Controllers/chatController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gnoiniyProject.Controllers
+{
+    public class chatController : Controller
+    {
+        // GET: chat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult main()
+        {
+            return View();
+        }
+    }
+}
